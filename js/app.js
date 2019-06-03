d3528f63db5a9476bb35db4db4c6b4bc38a51324
Igor Wysocki <narghar@gmail.com>
2019-06-03 23:33:47 +0200

    add styling to bottom icons and volume control

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
+/////  PLAYER ROTATE /////
 document.querySelectorAll(".flip").forEach(item => {
-  item.addEventListener("click", function(){
-     document.querySelector(".flip-player").classList.toggle("rotate");
+   item.addEventListener("click", function () {
+      document.querySelector(".flip-player").classList.toggle("rotate");
 
-  });
-  });
+   });
+});
+
+/////  VOLUME TRACK /////
+document.querySelector(".volume-range").oninput = function() {
+   this.style.background = 'linear-gradient(to right, #fff 0%, #fff '+this.value +'%, #ed5e74 ' + this.value + '%, #ed5e74 100%)'
+ };
