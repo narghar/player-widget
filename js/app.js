5a014c58bf08a2092e37c991991a4ba85996e271
Igor Wysocki <narghar@gmail.com>
2019-06-07 18:54:46 +0200

    fix  rotate glitches in Edge

--- js/app.js
+++ js/app.js
@@ -1,12 +1,27 @@
 /////  PLAYER ROTATE /////
+let frontSide = true;
 document.querySelectorAll(".flip").forEach(item => {
+
+
    item.addEventListener("click", function () {
       document.querySelector(".flip-player").classList.toggle("rotate");
+
+     // Edge glitches fix
+
+        if (frontSide) {
+         setTimeout(function () {
+      document.querySelector('.playlist-inner').classList.toggle("edge-scroll");
+         }, 400);
+      } else {
+      document.querySelector('.playlist-inner').classList.toggle("edge-scroll");
+
+      }
+      frontSide = !frontSide;
 
    });
 });
 
 /////  VOLUME TRACK /////
-document.querySelector(".volume-range").oninput = function() {
-   this.style.background = 'linear-gradient(to right, #fff 0%, #fff '+this.value +'%, #ed5e74 ' + this.value + '%, #ed5e74 100%)'
- };
+document.querySelector(".volume-range").oninput = function () {
+   this.style.background = 'linear-gradient(to right, #fff 0%, #fff ' + this.value + '%, #ed5e74 ' + this.value + '%, #ed5e74 100%)'
+};
